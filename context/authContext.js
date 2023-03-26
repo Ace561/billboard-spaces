6edea849b8dd10b529ba03e22308ec903880d2d9
edahmitchel <edahmitchel@gmail.com>
2023-03-26 08:47:12 +0100

    removed async storage

--- context/authContext.js
+++ context/authContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import axios from 'axios';
 import { API_BASE_URL } from '../config';
 
@@ -10,23 +10,12 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // Check if user is already logged in
-    AsyncStorage.getItem('user')
-      .then(userString => {
-        if (userString) {
-          setUser(JSON.parse(userString));
-        }
-        setLoading(false);
-      })
-      .catch(error => console.log(error));
-  }, []);
-
+  
   const register = (username, email, password) => {
     return axios.post(`${API_BASE_URL}/register`, { username, email, password })
       .then(response => {
         setUser(response.data.user);
-        AsyncStorage.setItem('user', JSON.stringify(response.data.user));
+        
       });
   };
 
@@ -34,13 +23,13 @@ export const AuthProvider = ({ children }) => {
     return axios.post(`${API_BASE_URL}/login`, { email, password })
       .then(response => {
         setUser(response.data.user);
-        AsyncStorage.setItem('user', JSON.stringify(response.data.user));
+      
       });
   };
 
   const logout = () => {
     setUser(null);
-    AsyncStorage.removeItem('user');
+    
   };
 
   if (loading) {
